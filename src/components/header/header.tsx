d7a8b66874dbe7fe9fe6894a23c189fdc1c25e92
Diego ribeiro <Diegoh.developer@gmail.com>
2024-06-05 21:06:42 -0300

    feat: Finalizando estilo do header;

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,20 +1,25 @@
 import Image from "next/image";
-import styles from './header.module.scss';
+import styles from "./header.module.scss";
 
-export default function Header(){
-    return(
-        <header className={styles.header}>
-            <nav className={styles.nav}>
-                <div className={styles.logo}>
-                    <h1>MKS</h1>
-                    <h6>Sistemas</h6>
-                </div>
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <div className={styles.logo}>
+          <h1>MKS</h1>
+          <h6>Sistemas</h6>
+        </div>
 
-                <div>
-                 <Image src="/carrinho.png" width={18} height={18} alt="carrinho de compra"/>
-                </div>
-                
-            </nav>
-        </header>
-    )
+        <button className={styles.buttonCar}>
+          <Image
+            src="/carrinho.png"
+            width={18}
+            height={18}
+            alt="carrinho de compra"
+          />
+          <span>0</span>
+        </button>
+      </nav>
+    </header>
+  );
 }
