4e0c985c8ccf15b7ba4bfb4ec68d58a863c85826
Diego ribeiro <Diegoh.developer@gmail.com>
2024-06-06 21:00:47 -0300

    feat: componente footer;

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,8 @@
+import styles from './footer.module.scss';
+export function Footer(){
+    return (
+        <footer className={styles.footer}>
+            <p>MKS sistemas © Todos os direitos reservados</p>
+        </footer>
+    )
+}
