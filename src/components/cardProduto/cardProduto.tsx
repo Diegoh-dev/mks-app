d5eaa2a26de9f47e846669296f9eb11eb2f9f545
Diego ribeiro <Diegoh.developer@gmail.com>
2024-06-07 14:00:34 -0300

    media screen;

--- src/components/cardProduto/cardProduto.tsx
+++ src/components/cardProduto/cardProduto.tsx
@@ -24,14 +24,7 @@ export function CardProduto({ produto }: Props) {
   return (
     <div className={styles.card}>
       <Image src={produto.photo} height={138} width={100} alt={produto.name} />
-      {/* <div
-        style={{
-          width: "110px",
-          height: "138px",
-          backgroundColor: "#373737",
-        }}
-      ></div> */}
-
+     
       <div className={styles.produtoPreco}>
         <span className={styles.nomeProduto}>{produto.name}</span>
         <div className={styles.preco}>R${produto.price}</div>
